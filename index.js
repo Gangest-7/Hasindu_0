3db57dea866c5b7882b91d43921646f46b403c9a
Gangest-7 <nethsaraadeesh3@gmail.com>
2026-09-02 13:19:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason
+} = require("@whiskeysockets/baileys");
+
+const pino = require("pino");
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("./session");
+
+  const sock = makeWASocket({
+    auth: state,
+    logger: pino({ level: "silent" }),
+    printQRInTerminal: false
+  });
+
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
+    if (connection === "open") {
+      console.log("✅ HASINDU MD BOT CONNECTED!");
+    }
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+
+      console.log("❌ Connection closed.");
+
+      if (shouldReconnect) {
+        startBot();
+      }
+    }
+  });
+}
+
+startBot();
