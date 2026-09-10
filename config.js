439d022c550803faa1c5c62e17fcbf1b66633a4b
Gangest-7 <nethsaraadeesh3@gmail.com>
2026-09-10 20:42:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   MODE: "public",
   VERSION: "3.0.0 Beta",
 
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "~7qTOADbEtkW3kIk",
   ALIVE_IMG: process.env.ALIVE_IMG || "",
 
   AUTO_READ: false,
